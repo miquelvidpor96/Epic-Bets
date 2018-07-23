1ed1ac807d034dbf6ddd1c2e2a9e8902e5c463fe
miquelvidal96 <miquelvidpor@gmail.com>
2018-07-23 18:21:44 +0200

    Agregado PantallaLogin base

--- EpicBets/Form1.Designer.cs
+++ EpicBets/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace EpicBets
 {
-    partial class Form1
+    partial class PatallaLogin
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // PatallaLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "PatallaLogin";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.PatallaLogin_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- EpicBets/Form1.cs
+++ EpicBets/Form1.cs
@@ -10,11 +10,20 @@ using System.Windows.Forms;
 
 namespace EpicBets
 {
-    public partial class Form1 : Form
+    public partial class PatallaLogin : Form
     {
-        public Form1()
+        public PatallaLogin()
         {
             InitializeComponent();
+        }
+
+        //Cuando carga la ventana...
+        private void PatallaLogin_Load(object sender, EventArgs e)
+        {
+            //Cambiar resolucion ventana
+            this.Size = new Size(300, 450);
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.CenterToScreen();
         }
     }
 }
